5dc5b73638e1e5db5cc0005e23db784f2dbb6a33
Alexandre Dumur <alexandre.dumur@epfl.ch>
2019-10-28 13:56:39 +0800

    rewrinting classes to improve readability

--- channel.py
+++ channel.py
@@ -6,9 +6,10 @@ This include a the sending delay, and transmission delay
 from abc import ABC, abstractmethod
 from time import time
 
+
 class Channel(ABC):
     def __init__(self, channelDesc):
-        self.channelDesc = channelDesc
+        pass
 
     @abstractmethod
     def send_frames(self, frames):
@@ -18,27 +19,19 @@ class Channel(ABC):
 
 
 class StableChannelNoWindow(Channel):
+    "Very Simple channel modulation where tansport layer is not modulated"
     def __init__(self, bandwith, sending_delay=0):
         self.bandwith = bandwith
         self.sending_delay = sending_delay
-
-        def delay(size):
-            return size / self.bandwith
-
-        super().__init__(delay)
-
-    def send(self, size):
-        return self.channelDesc(size)
 
     def send_frames(self, frames):
         """update availability field of Frames
         Return list of time that
         """
         for f in frames:
-            self.send(f.size)
-            f.availability = time() + self.send(f.size) + self.sending_delay
+            f.availability = time() + f.size / self.bandwith + self.sending_delay
 
-    def changeBandwith(bandwith):
+    def changeBandwith(self, bandwith):
         self.bandwith = bandwith
 
 # Maybe to simulate the window consider the length of frames
